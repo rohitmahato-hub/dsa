ec910f4aaec153240f2a7331b7f27fbb16014962
Rohit <rohitmahatoece@gmail.com>
2025-11-13 18:08:13 +0530

    frist comit

--- pattern_01/number_triangle.java
+++ pattern_01/number_triangle.java
@@ -5,7 +5,7 @@ public class number_triangle {
         System.out.println("enter a number:");
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++){
-            for(int j = 1;j <= i;j++){
+            for(int j = 1;j <= i ;j++){
                 System.out.print( j );
             }
             System.out.println();
