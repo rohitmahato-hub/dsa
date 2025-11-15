b5d608e97d8e46efdc804a74d6568d7f87737820
Rohit <rohitmahatoece@gmail.com>
2025-11-15 16:26:07 +0530

    frist commit

---
+++ pattern_01/increasing_number_triangle.java
@@ -0,0 +1,12 @@
+public class increasing_number_triangle {
+    public static void main(String[] args) {
+        int n = 1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print( n + " " );
+                n = n+1;
+            }
+            System.out.println();
+        }
+    }
+}
