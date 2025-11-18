8a614505a5b59b4353f5cbf86afd81f392130565
Rohit <rohitmahatoece@gmail.com>
2025-11-18 17:06:59 +0530

    frist commit

---
+++ pattern_01/SymmetricVoid.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class SymmetricVoid{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+        
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
