a9100d6c394315bbdf929a7a96d890755e1b9a36
Rohit <rohitmahatoece@gmail.com>
2025-11-23 16:35:46 +0530

    frist commit

---
+++ BitManipulation_02/print_bits.java
@@ -0,0 +1,10 @@
+public class print_bits {
+      public static void main(String[] args){
+        printBits(55);
+      }
+      public static void printBits(int num){
+        for(int i=7;i>=0;i--){
+            System.out.print((num>>i) & 1);
+        }
+      }
+}
