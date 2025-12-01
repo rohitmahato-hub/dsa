3a03b6fb0a16b267eafd79be4ef8ea637fe2898d
Rohit <rohitmahatoece@gmail.com>
2025-12-01 21:21:05 +0530

    minimum flips to make a OR b equal to c

---
+++ BitManipulation_02/question07.java
@@ -0,0 +1,27 @@
+public class question07 {
+    public static void main(String[] args) {
+        int a = 4, b = 2, c = 7;
+        int flips = flip(a,b,c);
+        System.out.println(flips);
+        
+    }
+    public static int flip(int a,int b, int c){
+        int flip = 0;
+
+        while( a!=0 || b!=0 || c!=0){
+            if((c & 1) == 1) {
+                if((a & 1) == 0 && (b & 1) == 0){
+                    flip++;
+                }else{
+                    if((a & 1) == 1) flip++;
+                    if((b & 1) == 1) flip++;
+                }
+                
+            }
+            a = a >> 1;
+            b = b >> 1;
+            c = c >> 1;
+        }
+        return flip;
+    }
+}
