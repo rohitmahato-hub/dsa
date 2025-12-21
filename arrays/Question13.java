104d0a969eb39623b4eb0d44c43203bd53f91cde
Rohit <rohitmahatoece@gmail.com>
2025-12-21 19:35:30 +0530

    remove duplicates from the sorted array

---
+++ arrays/Question13.java
@@ -0,0 +1,13 @@
+public class Question13 {
+    public static void main(String[] args) {
+        int arr[] = {0,0,1,1,1,2,2,3,3,4};
+        int k = 1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] != arr[i-1]){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        System.out.println(k);
+    }
+}
