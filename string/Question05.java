ba3b9eafe4ab230db531fb403f53d918e53ee397
Rohit <rohitmahatoece@gmail.com>
2026-02-26 20:58:23 +0530

    check isomorphic Strings

--- string/Question05.java
+++ string/Question05.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 
 public class Question05 {
     public static void main(String[] args) {
@@ -6,28 +5,49 @@ public class Question05 {
         String t = "b23";
         System.out.println(isIsomorphic(s,t));
     }
-    public static boolean isIsomorphic(String s, String t) {
-       HashMap<Character,Character> sToTMap = new HashMap<>();
-       HashMap<Character,Character> tToSMap = new HashMap<>();
-       StringBuilder sbS = new StringBuilder(s);
-       StringBuilder sbT = new StringBuilder(t);
+    // public static boolean isIsomorphic(String s, String t) {
+    //    HashMap<Character,Character> sToTMap = new HashMap<>();
+    //    HashMap<Character,Character> tToSMap = new HashMap<>();
+    //    StringBuilder sbS = new StringBuilder(s);
+    //    StringBuilder sbT = new StringBuilder(t);
 
-       for(int i=0;i<s.length();i++){
-            char s1 = sbS.charAt(i);
-            char t1 = sbT.charAt(i);
+    //    for(int i=0;i<s.length();i++){
+    //         char s1 = sbS.charAt(i);
+    //         char t1 = sbT.charAt(i);
 
-            if(!sToTMap.containsKey(s1)){
-                sToTMap.put(s1,t1);
-            }
-            if(!tToSMap.containsKey(t1)){
-                tToSMap.put(t1,s1);
-            }
+    //         if(!sToTMap.containsKey(s1)){
+    //             sToTMap.put(s1,t1);
+    //         }
+    //         if(!tToSMap.containsKey(t1)){
+    //             tToSMap.put(t1,s1);
+    //         }
 
-            if(sToTMap.get(s1)!=t1 || tToSMap.get(t1)!=s1){
+    //         if(sToTMap.get(s1)!=t1 || tToSMap.get(t1)!=s1){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    public  static boolean isIsomorphic(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if(mapST[c1] != mapTS[c2]) {
                 return false;
             }
+
+            mapST[c1] = i + 1;
+            mapTS[c2] = i + 1;
         }
+
         return true;
     }
-}
+    }
 
