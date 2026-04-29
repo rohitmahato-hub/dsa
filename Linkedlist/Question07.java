174909a09e28f837a5e3521ccd545680d5a96b2a
Rohit <rohitmahatoece@gmail.com>
2026-04-29 22:29:21 +0530

    remove nth node from the end

--- Linkedlist/Question07.java
+++ Linkedlist/Question07.java
@@ -16,7 +16,7 @@ public class Question07 {
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+        //head.next.next.next.next = new ListNode(5);
 
         ListNode res = removeNthFromEnd(head, n);
 
