2f88a4b917de4e36b7d412edff25ff6c0bd35aa1
Rohit <rohitmahatoece@gmail.com>
2026-05-01 20:20:10 +0530

    swap nodes in pairs in a list

---
+++ Linkedlist/Question12.java
@@ -0,0 +1,46 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+public class Question12 {
+    public static void main (String agrs[]){
+         ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = swapPairs(head);
+         ListNode curr = head;
+         while(curr != null){
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+    public static ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        
+        while(prev.next != null && prev.next.next != null){
+           ListNode first = prev.next;
+           ListNode second = first.next;
+
+           first.next = second.next;
+           second.next = first;
+           prev.next = second;
+
+            prev = first;
+        }
+        return dummy.next;
+    }
+}
