8957a6f55690bef17b4da479ea468717de2b5576
Rohit <rohitmahatoece@gmail.com>
2026-05-15 17:38:02 +0530

    another apporach

--- string/Question16.java
+++ string/Question16.java
@@ -6,17 +6,28 @@ public class Question16 {
         System.out.println(strStr(haystack, needle));
     }
     public static int strStr(String haystack, String needle) {
-       int n = haystack.length();
-       int m = needle.length();
-       for(int i = 0; i <= n-m;i++){
-        int j = 0;
-        while(j<m && haystack.charAt(j+i) == needle.charAt(j)){
-            j++;
-        }
-        if(j == m){
+    //    int n = haystack.length();
+    //    int m = needle.length();
+    //    for(int i = 0; i <= n-m;i++){
+    //     int j = 0;
+    //     while(j<m && haystack.charAt(j+i) == needle.charAt(j)){
+    //         j++;
+    //     }
+    //     if(j == m){
+    //         return i;
+    //     }
+    //    }
+    //    return -1;
+
+
+    //another apporachh
+     for(int i=0;i<haystack.length()-needle.length()+1;i++){
+        if(haystack.charAt(i) == needle.charAt(0)){
+           if(haystack.substring(i,needle.length() +i).equals(needle)){
             return i;
+           }
         }
-       }
-       return -1;
+    }
+    return -1;
     }
 }
