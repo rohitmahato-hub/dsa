2708061296b31a7e3d8bf2bc97d79f00f2559f3d
Rohit <rohitmahatoece@gmail.com>
2026-06-08 10:34:22 +0530

    refactor minAddToMakeValid method to remove stack implementation and simplify logic

--- Stack/Question15.java
+++ Stack/Question15.java
@@ -4,21 +4,51 @@ public class Question15 {
         String s = "()))((";
         System.out.println(minAddToMakeValid(s));
     }
-     public static int minAddToMakeValid(String s) {
-      int count = 0;
-      Stack<Character> stack = new Stack<>();
-      for(int i = 0;i<s.length();i++){
-        char ch = s.charAt(i);
-         if(ch == '('){
-            stack.push(ch);
-         }else{
-            if(!stack.isEmpty()){
-                stack.pop();
-            }else{
-                count++;
+    //  public static int minAddToMakeValid(String s) {
+    //   int count = 0;
+    //   Stack<Character> stack = new Stack<>();
+    //   for(int i = 0;i<s.length();i++){
+    //     char ch = s.charAt(i);
+    //      if(ch == '('){
+    //         stack.push(ch);
+    //      }else{
+    //         if(!stack.isEmpty()){
+    //             stack.pop();
+    //         }else{
+    //             count++;
+    //         }
+    //      }
+    //   }
+    //   return count + stack.size();  
+    // }
+
+    //without stack
+    public int minAddToMakeValid(String s) {
+
+        int open = 0;
+        int additions = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if(ch == '(') {
+
+                open++;
+
+            } else {
+
+                if(open > 0) {
+
+                    open--;
+
+                } else {
+
+                    additions++;
+                }
             }
-         }
-      }
-      return count + stack.size();  
+        }
+
+        return additions + open;
     }
 }
