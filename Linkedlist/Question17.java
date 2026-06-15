5f2448989a337e03ed3f2a42065c382ee7072e5b
Rohit <rohitmahatoece@gmail.com>
2026-06-15 13:18:52 +0530

    delete the middle node of the linkedlist

---
+++ Linkedlist/Question17.java
@@ -0,0 +1,51 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Question17 {
+    public static void main(String [] args){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(3);
+        head.next.next = new ListNode(4);
+        head.next.next.next = new ListNode(7);
+        head.next.next.next.next = new ListNode(1);
+        head .next.next.next.next.next = new ListNode(2);
+        head .next.next.next.next.next.next = new ListNode(6);
+        ListNode result = deleteMiddle(head);
+        while (result != null) {
+            System.out.print(result.val);
+
+            if (result.next != null) {
+                System.out.print(" -> ");
+            }
+
+            result = result.next;
+        }
+
+    }
+    public static ListNode deleteMiddle(ListNode head) {
+
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+}
