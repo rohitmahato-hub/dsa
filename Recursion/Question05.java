81e545683aa2756cdecada4bc8a82cb84afd809c
Rohit <rohitmahatoece@gmail.com>
2026-07-08 21:16:26 +0530

    binary search in recursive way

---
+++ Recursion/Question05.java
@@ -0,0 +1,30 @@
+public class Question05 {
+    public static void main(String args[]){
+       int[] nums = {-1,0,3,5,9,12};
+       int target = 9;
+       int res = search(nums, target);
+       System.out.println(res);
+    }
+     public static int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    private static int binarySearch(int[] nums, int target, int start, int end) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (target < nums[mid]) {
+            return binarySearch(nums, target, start, mid - 1);
+        }
+
+        return binarySearch(nums, target, mid + 1, end);
+    }
+}
